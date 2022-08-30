25d52ec51f2945d25a704714b93c0f1bc9700e96
austrez <austrez@yandex.com>
2022-08-30 20:16:11 +0500

    add prettier; change eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,20 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
-  ],
-  "parserOptions": {
-    "ecmaVersion": "latest"
-  }
-}
+	root: true,
+	env: {
+		es2022: true,
+		node: true,
+	},
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/eslint-config-typescript/recommended',
+		'prettier',
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+};
