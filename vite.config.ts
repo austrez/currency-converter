5f5a062702d3305e2cf2cecb1b6051552aa6ae52
austrez <austrez@yandex.com>
2022-08-30 23:55:49 +0500

    add vuetify

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,19 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
-// https://vitejs.dev/config/
+import vuetify from 'vite-plugin-vuetify';
+import Components from 'unplugin-vue-components/vite';
+import { Vuetify3Resolver } from 'unplugin-vue-components/resolvers';
+
 export default defineConfig({
-	plugins: [vue(), vueJsx()],
+	plugins: [
+		vue(),
+		vuetify(),
+		vueJsx(),
+		Components({
+			resolvers: [Vuetify3Resolver()],
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
