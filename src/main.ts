e2349525dd27075d52df0244d1923c62c3a131d7
austrez <austrez@yandex.com>
2022-08-31 16:42:30 +0500

    Revert "add vuetify"
    This reverts commit 5f5a062702d3305e2cf2cecb1b6051552aa6ae52.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import vuetify from './plugins/vuetify';
 import './styles/_index.scss';
 
-const app = createApp(App);
-
-app.use(vuetify);
-app.mount('#app');
+createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
-import vuetify from 'vite-plugin-vuetify';
-import Components from 'unplugin-vue-components/vite';
-import { Vuetify3Resolver } from 'unplugin-vue-components/resolvers';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		vuetify(),
-		vueJsx(),
-		Components({
-			resolvers: [Vuetify3Resolver()],
-		}),
-	],
+	plugins: [vue(), vueJsx()],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
